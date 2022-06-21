532fc6fd39bacf4d779ba8fc957c02df3e2c0bf7
lordson-silvergit <lordsonochuko5@gmail.com>
2022-06-21 16:02:49 +0100

    Further more on objects

--- objects.js
+++ objects.js
@@ -7,3 +7,18 @@ function HouseKeeper(name, age, yearsOfExperience, languages) {
     this.languages = languages;
 };
 var houseKeeper1 = new HouseKeeper("Annie", 22, 2, ["English", "Yoruba"]);
+
+
+function Apartment(name, numOfRooms) {
+    this.name = name;
+    this.numOfRooms = numOfRooms;
+};
+var house1 = new Apartment("Shalom Place", 20);
+var house2 = new Apartment("Eagles Pride", 22);
+
+
+function User(userName, email, password) {
+    this.email = email;
+    this.password = password;
+    this.userName = userName;
+};
