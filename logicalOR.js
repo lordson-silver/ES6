a9c7acddf0b874a2600c99a5d1174ecee9fcde99
lordson-silver <lordsonochuko5@gmail.com>
2022-07-08 11:03:23 +0100

    Add logicalOR.js

---
+++ logicalOR.js
@@ -0,0 +1,20 @@
+// Logical OR assignment ||
+
+const song = {
+  title: "", 
+  album: "Love Damini", 
+  artiste: "Burna Boy",
+  duration: 186
+};
+
+song.duration ||= 160;
+console.log(song.duration);
+// Expected output 186
+
+song.title ||= "Wild Dreams";
+console.log(song.title);
+// Expected output "Wild Dreams"
+
+console.log(song.artiste + " - " + song.album)
+
+// The logical OR assignment (x ||= y) operator only assigns if x is falsy. 
