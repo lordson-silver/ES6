47e1b90613243b4382d233dace1d4436d754733e
lordson-silver <lordsonochuko5@gmail.com>
2022-07-15 00:01:19 +0100

    Fix: made some changes

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ let Car = {
   name : "Tesla",
   price : 50000,
   hasAutoPilot : function() {
-    return(console.log(Has AutoPilot)); // Has AutoPilot
+    return(console.log("Has AutoPilot")); // Has AutoPilot
   }
 };
 
@@ -15,9 +15,25 @@ function HouseKeeper(name, age, yearsOfExperience, languages) {
     this.yearsOfExperience = yearsOfExperience; 
     this.languages = languages; 
 };
+
+// get full summary using prototype
+HouseKeeper.prototype.getFullSummary = function() {
+    return(`${this.name} has ${this.age} years of experience and knows ${this.languages}`);
+}
+
+
+// function getFullSummary(obj) {
+//     return(`${obj.name} is a ${obj.age} year old ${obj.yearsOfExperience} years experience housekeeper with ${obj.languages} languages`);
+// };
+
+
+
     var houseKeeper1 = new HouseKeeper("Annie", 22, 2, ["English", "Yoruba"]); 
     // Annie is a housekeeper with 2 years of experience and knows English and Yoruba.
 
+// log annie.getFullSummary()
+console.log(houseKeeper1.getFullSummary());
+// console.log(getFullSummary(houseKeeper1));
 
 function Apartment(name, numOfRooms) {
     this.name = name;
