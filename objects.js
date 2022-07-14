54288bdee133e989b147383316a71f5c7c36e84c
lordson-silver <lordsonochuko5@gmail.com>
2022-07-14 15:46:56 +0100

    Add: Added comments

--- objects.js
+++ objects.js
@@ -5,25 +5,26 @@ let Car = {
   name : "Tesla",
   price : 50000,
   hasAutoPilot : function() {
-    return(console.log(Has AutoPilot);)
+    return(console.log(Has AutoPilot)); // Has AutoPilot
   }
 };
 
 function HouseKeeper(name, age, yearsOfExperience, languages) {
-    this.name = name;
-    this.age = age;
-    this.yearsOfExperience = yearsOfExperience;
-    this.languages = languages;
+    this.name = name; 
+    this.age = age; 
+    this.yearsOfExperience = yearsOfExperience; 
+    this.languages = languages; 
 };
-var houseKeeper1 = new HouseKeeper("Annie", 22, 2, ["English", "Yoruba"]);
+    var houseKeeper1 = new HouseKeeper("Annie", 22, 2, ["English", "Yoruba"]); 
+    // Annie is a housekeeper with 2 years of experience and knows English and Yoruba.
 
 
 function Apartment(name, numOfRooms) {
     this.name = name;
     this.numOfRooms = numOfRooms;
 };
-var house1 = new Apartment("Shalom Place", 20);
-var house2 = new Apartment("Eagles Pride", 22);
+var house1 = new Apartment("Shalom Place", 20);  // Shalom Place has 20 rooms.
+var house2 = new Apartment("Eagles Pride", 22);  
 
 
 function User(userName, email, password) {
