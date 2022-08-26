80d1670dec42ab8500d0f7293771dea276ba06cc
lordson-silver <lordsonochuko5@gmail.com>
2022-08-26 13:58:38 +0100

    Started Version 2.0 of the ES6 project

--- alert.js
+++ alert.js
@@ -1,4 +1,4 @@
 // Create a program, that ask's users to input their name and display it back to them
 // like so : Hello Lordson
-
-alert("Hello " + prompt ("What is your name?"));
+const readlineSync = require('readline-sync');
+console.log("Hello " + readlineSync.question("What is your name? "));
