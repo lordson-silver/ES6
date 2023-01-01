571b8cf990ff8b86a7f9931f715db279d119fc5d
lordson-silver <lordsonochuko5@gmail.com>
2023-01-01 08:46:14 +0100

    Happy new year commit

---
+++ happyNewYear.js
@@ -0,0 +1,11 @@
+/*
+ * Create a terminal countdown, that checks the cutrent year
+ * if > 2022 then
+ * &show how many days and hours and minutes and seconds till 2023
+ * use this format "Its X_days Y_hours Z_Minutes P_Seconds till 2023"
+ * If 2023 then
+ * show how long since
+ * show how many days and hours and minutes and seconds since 2023
+ * use this format "Its X_days Y_hours Z_Minutes P_Seconds since 2023"
+ * Make use of google, functions, conditionals, not to mention variables
+ * */
